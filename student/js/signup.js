02384f5120ea2258a0f51e070184c94c31390d30
Radhe Shyam <radheshyam@Votercircles-MacBook-Air.local>
2023-02-15 13:17:23 +0530

    added files

---
+++ student/js/signup.js
@@ -0,0 +1,46 @@
+
+document.querySelector("#submit").addEventListener("click", function(){
+    let user=JSON.parse(localStorage.getItem("pdi_stu_info"))|| []
+
+    let obj={
+        username:document.querySelector("#username").value,
+        email:document.querySelector("#email").value,
+        password:document.querySelector("#password").value,
+        profilepic:document.querySelector("#profilepic").value,
+        gender:document.querySelector("#gender").value,
+        role:document.querySelector("#role").value,
+        dob:document.querySelector("#dob").value,
+        state:document.querySelector("#state").value,
+        city:document.querySelector("#city").value,
+        mobile:document.querySelector("#mobile").value,
+        toj:new Date()
+
+    }
+    let emailCheck=obj.email.split("@")[1].split(".")[1]
+    console.log(emailCheck);
+    if(emailCheck=="in" || emailCheck=="com")
+    {
+        let existingUser=user.find((el)=>el.email==obj.email)
+        if(existingUser){
+            alert("User already exists")
+        }
+        else{
+            user.push(obj)
+            localStorage.setItem("pdi_stu_info",JSON.stringify(user))
+            window.location.href="../html/login.html"
+        }
+    }
+    else{
+        alert("This is not valid Email Id")
+    }
+    
+})
+
+let currDate=new Date()
+let year=currDate.getFullYear()
+let month=currDate.getMonth()+1 <10? "0"+(currDate.getMonth()+1):currDate.getMonth()+1
+let date=currDate.getDate()<10? "0"+(currDate.getDate()):currDate.getDate()
+let maxDate=year+"-"+month+"-"+date
+let dob=document.querySelector("#dob")
+dob.setAttribute("max",maxDate)
+console.log(maxDate);
